39fa164f4575951fd0b2fe9c764adcef3dc24154
maixiangCat <valojia123@gmail.com>
2025-07-02 21:26:43 +0800

    chore:增加了测试覆盖率

---
+++ packages/components/index.test.tsx
@@ -0,0 +1,27 @@
+import type { Plugin } from "vue";
+import { describe, it, expect } from "vitest";
+import {
+  MxmAlert,
+  MxmButton,
+  MxmButtonGroup,
+  MxmCollapse,
+  MxmCollapseItem,
+  MxmIcon
+} from './index'
+import { map, get } from "lodash-es";
+
+const components = [
+  MxmAlert,
+  MxmButton,
+  MxmButtonGroup,
+  MxmCollapse,
+  MxmCollapseItem,
+  MxmIcon
+] as Plugin[]
+
+describe("components/index.ts", () => {
+  it.each(map(components, (c) => [get(c, "name") ?? "", c]))("%s should be exported", (_, component) => {
+    expect(component).toBeDefined();
+    expect(component.install).toBeDefined()
+  });
+});
